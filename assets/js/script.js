90bb3b38f62fbcf2215789ffca502272e3cf3260
MiltonB <milton1villa23@gmail.com>
2021-05-16 20:03:51 -0400

    prueba trabar con nosotros

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,102 @@
         // console.log(`${w.location.origin}/assets/pages/${ruta.archivo}`);
     }
 })(document, window);
+
+
+
+// script trabajar con nosotros
+
+// (function () {
+//     function cargarDatosDesdeLocalStorage() {
+//         let retorno = [];
+//         if (localStorage.getItem('personas') !== null) {
+//             retorno = JSON.parse(localStorage.getItem('personas'));
+//         }
+//         return retorno;
+//     } 
+//     const datosUsuario = cargarDatosDesdeLocalStorage();
+//     const formulario = document.getElementsByTagName('form')[0];
+//     const tbody = document.getElementById('contenido');
+//     formulario.addEventListener('submit', function(evento) {
+//         evento.preventDefault();
+
+//     });
+    
+//     formulario.addEventListener('submit', function (evento) {
+//         const rut = formulario.rut.value;
+//         const nombre = formulario.nombre.value;
+//         const apellido = formulario.apellido.value;
+//         const telefono = formulario.telefono.value;
+//         const correoelectronico = formulario.correoelectronico.value;
+//         const cargo = formulario.cargo.value;
+//         const objetoPersonaTemporal = {
+//             rut: rut,
+//             nombre: nombre,
+//             apellido: apellido,
+//             telefono: telefono,
+//             correoelectronico: correoelectronico,
+//             cargo: cargo,
+//         };
+
+//         anadirElementoALaListaYLocalStorange(objetoPersonaTemporal);
+//         anadirElementoATabla(objetoPersonaTemporal);
+//         alert('Haz sido registrado');
+//         formulario.reset();
+
+//     });
+//     function anadirElementoALaListaYLocalStorange(persona){
+//         datosUsuario.push(persona);
+//         localStorage.setItem('personas', JSON.stringify(datosUsuario));
+//     }
+
+//     function anadirElementoATabla(hijoAAnadir) {
+//         const trTemporal = document.createElement('tr');
+
+//         const tdRut = document.createElement('td');
+//         tdRut.innerText = hijoAAnadir.rut;
+
+//         const tdNombre = document.createElement('td');
+//         tdNombre.innerText = hijoAAnadir.nombre;
+
+//         const tdApellido = document.createElement('td');
+//         tdApellido.innerText = hijoAAnadir.apellido;
+
+//         const tdTelefono = document.createElement('td');
+//         tdTelefono.innerText = hijoAAnadir.telefono;
+
+//         const tdCorreoelectronico = document.createElement('td');
+//         tdCorreoelectronico.innerText = hijoAAnadir.correoelectronico;
+
+//         const tdCargo = document.createElement('td');
+
+//         tdCargo.innerText= hijoAAnadir.cargo;
+//         tdCargo.addEventListener('click', function() {
+//             console.log('oprimido :', hijoAAnadir.cargo);
+
+//         });
+
+//         trTemporal.appendChild(tdRut);
+//         trTemporal.appendChild(tdNombre);
+//         trTemporal.appendChild(tdApellido);
+//         trTemporal.appendChild(tdTelefono);
+//         trTemporal.appendChild(tdCorreoelectronico);
+//         trTemporal.appendChild(tdCargo);
+
+//         tbody.appendChild(trTemporal);
+//     }
+
+//         function pintarTablaInicio() {
+//             for (let i = 0; i < datosUsuario.length; i++) {
+//                 anadirElementoATabla(datosUsuario[i]);
+//             }
+//         }
+//         document.addEventListener('readystatechange', function () {
+//             if (document.readyState === 'interactive') {
+//                 pintarTablaInicio();
+//             }
+
+//     });
+
+
+   
+// }());
