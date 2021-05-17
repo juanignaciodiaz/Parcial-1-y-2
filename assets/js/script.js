891a2ab55e2d5e61b359457e49eacfbd4f788706
Camilo_Ts <camil.tapias@gmail.com>
2021-05-17 01:00:20 -0400

    Se implemento js a la pagina registro para prevenir el evento submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 (function (d, w) {
+    // VARIABLES
+    const contenedor = d.getElementById('contenido-pagina');
+    let formulario = '';
     const rutas = {
-        '/':{
+        '/': {
             archivo: 'index.html',
             titulo: 'Principal'
         },
@@ -11,43 +14,73 @@
         '/comentarios': {
             archivo: 'comentarios.html',
             titulo: 'Comentarios'
+        },
+        '/registrar': {
+            archivo: 'registrar.html',
+            titulo: 'Registrarse'
         }
     }
 
     const productos = [];
+
+    // ======  EVENTOS ======
+    
+
     d.addEventListener('readystatechange', function () {
         if (d.readyState === 'interactive') {
-            console.log('Hola'); 
+            // console.log('Hola'); 
             iniciar();
-            
+
         }
     });
 
+
+    // ====== FUNCIONES ======
+
     function iniciar() {
-        w.addEventListener('hashchange', function(){
+        w.addEventListener('hashchange', function () {
             const ruta = w.location.hash;
-    
             cargarPagina(ruta);
-            console.log(ruta);
+            // eventosFormulario();
         });
         cargarPagina(w.location.hash);
+
     }
 
     function cargarPagina(r) {
         const ruta = rutas[r.substring(1)];
 
-        fetch(`${w.location.origin}/assets/pages/${ruta.archivo}`,{
+        fetch(`${w.location.origin}/assets/pages/${ruta.archivo}`, {
             method: 'GET'
-        }).then(function(respuesta){
+        }).then(function (respuesta) {
             return respuesta.text();
-        }).then(function(respuesta) {
-            const contenedor = d.getElementById('contenido-pagina');
+        }).then(function (respuesta) {
+            
             contenedor.innerHTML = '';
             contenedor.innerHTML = respuesta;
             d.title = ruta.titulo;
-            console.log(respuesta);
-        });
 
-        // console.log(`${w.location.origin}/assets/pages/${ruta.archivo}`);
+            if (w.location.hash == '#/registrar') {
+                formulario = contenedor.getElementsByTagName('form')[0];
+                console.log(formulario);
+
+                formulario.addEventListener('submit', function(e) {
+                    e.preventDefault();
+                    console.log('Camilo');
+                })
+            }
+            
+
+
+        });
+    }
+
+    function productosLocalStorageLista(producto) {
+        productos.push(producto);
+        localStorage.setItem('productos', JSON.stringify(productos));
+    }
+
+    function pintarProductos() {
+
     }
 })(document, window);
