56ccdc978a6d62c29affeef82b2e890c554cf61c
ReneRetamales <re.retamales@duocuc.cl>
2021-05-17 16:29:04 -0400

    Ahora se pueden agregar comentarios en directo a la página

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,11 +48,16 @@
             contenedor.innerHTML = respuesta;
             d.title = ruta.titulo;
             
-            if (ruta == '#/comentarios') {
-                let lista = contenedor.getElementById('comments-list');
-                formulario = contenedor.getElementsByTagName('form');
-            
-                crearComentario(formulario, lista)
+            if (w.location.hash == '#/comentarios') {
+                formulario = contenedor.getElementsByTagName('form')[0];
+                let contenido_lista = contenedor.querySelector('main section article div ul');
+                formulario.addEventListener('submit', function(e) {
+                    console.log(contenido_lista);
+                    e.preventDefault();
+
+                    crearComentario(formulario, contenido_lista);
+
+                });
             }
 
         });
@@ -62,7 +67,7 @@
 
     // Funciones de la página comentarios
 
-    function crearComentario(comentarios, lista) {
+    function crearComentario(formComentarios, contenido) {
         
         const lista = d.createElement('li');
         lista.setAttribute('class', 'comments-list');
@@ -79,42 +84,44 @@
         comment_avatar.appendChild(avatar_img);
 
         const comment_box = d.createElement('div');
-        comment_box.setAttribute('class', 'comment_box');
+        comment_box.setAttribute('class', 'comment-box');
         comment_main_level.appendChild(comment_box);
 
         const comment_head = d.createElement('div');
-        comment_head.setAttribute('class', 'comment_head');
+        comment_head.setAttribute('class', 'comment-head');
         comment_box.appendChild(comment_head);
 
         const comment_name = d.createElement('h6');
-        comment_name.setAttribute('class', 'comment_name');
+        comment_name.setAttribute('class', 'comment-name');
+        comment_name.innerText = 'Test1';
         comment_head.appendChild(comment_name);
-
-        // Arreglar
+        
+        
         const comment_date = d.createElement('span');
         comment_date.innerText = 'Hace una hora';
         comment_head.appendChild(comment_date);
 
         const comment_icon_like = d.createElement('i');
-        comment_icon_like.setAttribute('class', 'comment_icon_like');
+        comment_icon_like.setAttribute('class', 'fas fa-thumbs-up');
         comment_head.appendChild(comment_icon_like);
 
         const comment_icon_edit = d.createElement('i');
-        comment_icon_edit.setAttribute('class', 'comment_icon_edit');
+        comment_icon_edit.setAttribute('class', 'fas fa-pencil-alt');
         comment_head.appendChild(comment_icon_edit);
 
         const comment_icon_share = d.createElement('i');
-        comment_icon_share.setAttribute('class', 'comment_icon_share');
+        comment_icon_share.setAttribute('class', 'fas fa-share-alt');
         comment_head.appendChild(comment_icon_share);
 
         const comment_content = d.createElement('div');
-        comment_content.setAttribute('class', 'comment_content');
-        comment_content.innerText = 'Comentario de prueba';
+        comment_content.setAttribute('class', 'comment-content');
+        comment_content.innerText = formComentarios.comentario.value;
         comment_box.appendChild(comment_content);
 
 
         lista.appendChild(comment_main_level);
-
+        contenido.appendChild(lista);
+        formComentarios.reset();
 
     }
 
