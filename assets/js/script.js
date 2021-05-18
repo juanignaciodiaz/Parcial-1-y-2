21c0faec7bafd736aedac7db381a1978f0b8c973
Juan Ignacio Diaz Hernandez <ignaciodiazh13@gmail.com>
2021-05-18 10:20:06 -0400

    Cambios reparacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 (function (d, w) {
     const rutas = {
-        '/':{
-            archivo: 'index.html',
+        '': {
+            archivo: 'principal.html',
             titulo: 'Principal'
         },
         '/productos': {
@@ -11,22 +11,29 @@
         '/comentarios': {
             archivo: 'comentarios.html',
             titulo: 'Comentarios'
+        },
+        '/Prueba': {
+            archivo: 'paginaprueba.html',
+            titulo: 'Pagina prueba'
         }
     }
+
+    setInterval(function(){mostrarhora()}, 1000);
 
     const productos = [];
     d.addEventListener('readystatechange', function () {
         if (d.readyState === 'interactive') {
-            console.log('Hola'); 
+            console.log('Hola');
             iniciar();
             
+
         }
     });
 
     function iniciar() {
-        w.addEventListener('hashchange', function(){
+        w.addEventListener('hashchange', function () {
             const ruta = w.location.hash;
-    
+
             cargarPagina(ruta);
             console.log(ruta);
         });
@@ -36,18 +43,28 @@
     function cargarPagina(r) {
         const ruta = rutas[r.substring(1)];
 
-        fetch(`${w.location.origin}/assets/pages/${ruta.archivo}`,{
+        fetch(`${w.location.origin}/assets/pages/${ruta.archivo}`, {
             method: 'GET'
-        }).then(function(respuesta){
+        }).then(function (respuesta) {
             return respuesta.text();
-        }).then(function(respuesta) {
+        }).then(function (respuesta) {
             const contenedor = d.getElementById('contenido-pagina');
             contenedor.innerHTML = '';
             contenedor.innerHTML = respuesta;
             d.title = ruta.titulo;
             console.log(respuesta);
         });
-
         // console.log(`${w.location.origin}/assets/pages/${ruta.archivo}`);
+    }
+    function mostrarhora(){
+        let momentoActual = new Date();
+        let hora = momentoActual.getHours();
+        let minuto = momentoActual.getMinutes();
+        let segundo = momentoActual.getSeconds();
+    
+        let horaImprimible = hora + ":" + minuto + ":" + segundo;
+    
+        const contenido_hora = document.getElementById('mostrarhora');
+        contenido_hora.innerText = horaImprimible ;
     }
 })(document, window);
