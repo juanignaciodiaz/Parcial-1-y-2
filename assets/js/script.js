a59f34e60bae152827bb7e11026df39cdc5d4c04
Camilo_Ts <camil.tapias@gmail.com>
2021-05-18 12:06:26 -0400

    Se implemento la pagina sin cargas a la pagina de registro y se implemento localStorage para crear usuarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 (function (d, w) {
     // VARIABLES
+    let idU = 0;
+    let idP = 0;
     const contenedor = d.getElementById('contenido-pagina');
     let formulario = '';
     const rutas = {
@@ -21,19 +23,17 @@
         }
     }
 
-    const productos = [];
+    let productos = [];
+
+    let usuarios = cargarUsuarios();
 
     // ======  EVENTOS ======
-    
-
     d.addEventListener('readystatechange', function () {
         if (d.readyState === 'interactive') {
-            // console.log('Hola'); 
             iniciar();
 
         }
     });
-
 
     // ====== FUNCIONES ======
 
@@ -41,7 +41,10 @@
         w.addEventListener('hashchange', function () {
             const ruta = w.location.hash;
             cargarPagina(ruta);
-            // eventosFormulario();
+
+            if (ruta == '#/registrar') {
+                console.log(formulario);
+            }
         });
         cargarPagina(w.location.hash);
 
@@ -55,31 +58,106 @@
         }).then(function (respuesta) {
             return respuesta.text();
         }).then(function (respuesta) {
-            
+
             contenedor.innerHTML = '';
             contenedor.innerHTML = respuesta;
             d.title = ruta.titulo;
 
             if (w.location.hash == '#/registrar') {
-                formulario = contenedor.getElementsByTagName('form')[0];
-                console.log(formulario);
 
-                formulario.addEventListener('submit', function(e) {
+                formulario = contenedor.getElementsByTagName('form')[0];
+
+                formulario.regUrlImagen.addEventListener('input', function () {
+                    let imagen = contenedor.getElementsByTagName('img')[0];
+
+                    if (formulario.regUrlImagen.value.length != '') {
+                        imagen.src = formulario.regUrlImagen.value;
+                    } else {
+                        imagen.src = 'assets/images/foto-incognito.jpg';
+                    }
+                })
+
+                formulario.addEventListener('submit', function (e) {
                     e.preventDefault();
-                    console.log('Camilo');
+
+                    crearUsuario(formulario)
                 })
             }
-            
+
 
 
         });
     }
 
+    // GUARDAR LOCALSTORAGE Y LISTA
+    function usuariosLocalStorageLista(usuario) {
+        usuarios.push(usuario);
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        idU++;
+    }
     function productosLocalStorageLista(producto) {
         productos.push(producto);
         localStorage.setItem('productos', JSON.stringify(productos));
     }
 
+    // FUNCIONES
+    function crearUsuario(user) {
+        
+        const regUsuario = user.regUsuario.value;
+        const nombre = user.regNombre.value;
+        const correo = user.regCorreo.value;
+        const contrasenia = user.regCon.value;
+        const rep_contrasenia = user.regRepCon.value;
+        const fecha_nac = user.fechaN.value;
+
+        let usuario = {
+            nomUsuario: regUsuario,
+            nombre: nombre,
+            correo: correo,
+            contrasenia: contrasenia,
+            fecha_nac: fecha_nac
+        }
+        console.log(usuario);
+        // if (validarNuevoUsuario(usuario)) {
+        usuariosLocalStorageLista(usuario);
+
+        // }
+        console.log(usuario.nombre);
+        console.log(usuarios);
+    }
+    function validarNuevoUsuario(usuario) {
+        let user = '';
+        let confirmacion = false;
+        if (usuario.nomUsuario.length <= 4) {
+            alert('Usuario');
+
+        }
+        if (usuario.nombre.length <= 4) {
+            alert('Nombre');
+        }
+
+        if (usuario.correo.length <= 4) {
+            alert('Correo');
+        }
+
+        if (usuario.contrasenia.length <= 4) {
+
+        }
+
+
+        user = usuario
+
+        return user;
+    }
+
+    function cargarUsuarios() {
+        let retorno = []
+        if (localStorage.getItem('usuarios') != null) {
+            retorno = JSON.parse(localStorage.getItem('usuarios'));
+        }
+        return retorno;
+    }
+    // PRODUCTOS
     function pintarProductos() {
 
     }
