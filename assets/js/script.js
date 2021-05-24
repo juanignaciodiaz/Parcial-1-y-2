36d183ec4dcbefb3a10bf156130e68a87f581a5f
MiltonB <milton1villa23@gmail.com>
2021-05-24 17:20:57 -0400

    se areglaron detalles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 (function (d, w) {
     const rutas = {
-        '/':{
+        '/': {
             archivo: 'index.html',
             titulo: 'Principal'
         },
@@ -12,26 +12,27 @@
             archivo: 'comentarios.html',
             titulo: 'Comentarios'
         },
-        '/contacto' : {
+        '/contacto': {
             archivo: 'contacto.html',
             titulo: 'Contacto'
         }
 
     }
 
+    let contenedor = d.getElementById('contenido-pagina');
     const productos = [];
     d.addEventListener('readystatechange', function () {
         if (d.readyState === 'interactive') {
-            console.log('Hola'); 
+            console.log('Hola');
             iniciar();
-            
+
         }
     });
 
     function iniciar() {
-        w.addEventListener('hashchange', function(){
+        w.addEventListener('hashchange', function () {
             const ruta = w.location.hash;
-    
+
             cargarPagina(ruta);
             console.log(ruta);
         });
@@ -41,116 +42,112 @@
     function cargarPagina(r) {
         const ruta = rutas[r.substring(1)];
 
-        fetch(`${w.location.origin}/assets/pages/${ruta.archivo}`,{
+        fetch(`${w.location.origin}/assets/pages/${ruta.archivo}`, {
             method: 'GET'
-        }).then(function(respuesta){
+        }).then(function (respuesta) {
             return respuesta.text();
-        }).then(function(respuesta) {
-            const contenedor = d.getElementById('contenido-pagina');
+        }).then(function (respuesta) {
+
             contenedor.innerHTML = '';
             contenedor.innerHTML = respuesta;
             d.title = ruta.titulo;
-            console.log(respuesta);
+
+            if (w.location.hash == '#/contacto') {
+
+                formulario = contenedor.getElementsByTagName('form')[0];
+
+                formulario.addEventListener('submit', function(e){
+                    e.preventDefault();
+                alert('Haz sido registrado');
+                    
+
+
+                });
+            }
+
         });
-
-        // console.log(`${w.location.origin}/assets/pages/${ruta.archivo}`);
     }
-})(document, window);
+}) (document, window);
 
 
-(function () {
-    function cargarDatosDesdeLocalStorage() {
-        let retorno = [];
-        if (localStorage.getItem('personas') !== null) {
-            retorno = JSON.parse(localStorage.getItem('personas'));
-        }
-        return retorno;
-    } 
-    const datosUsuario = cargarDatosDesdeLocalStorage();
-    const formulario = document.getElementsByTagName('form')[0];
-    const tbody = document.getElementById('contenido');
-    formulario.addEventListener('submit', function(evento) {
-        evento.preventDefault();
-
-    });
     
-    formulario.addEventListener('submit', function (evento) {
-        const rut = formulario.rut.value;
-        const nombre = formulario.nombre.value;
-        const apellido = formulario.apellido.value;
-        const telefono = formulario.telefono.value;
-        const correoelectronico = formulario.correoelectronico.value;
-        const cargo = formulario.cargo.value;
-        const objetoPersonaTemporal = {
-            rut: rut,
-            nombre: nombre,
-            apellido: apellido,
-            telefono: telefono,
-            correoelectronico: correoelectronico,
-            cargo: cargo,
-        };
-
-        anadirElementoALaListaYLocalStorange(objetoPersonaTemporal);
-        anadirElementoATabla(objetoPersonaTemporal);
-        alert('Haz sido registrado');
-        formulario.reset();
-
-    });
-    function anadirElementoALaListaYLocalStorange(persona){
-        datosUsuario.push(persona);
-        localStorage.setItem('personas', JSON.stringify(datosUsuario));
-    }
-
-
-    function anadirElementoATabla(contenidoTabla) {
-        const trTemporal = document.createElement('tr');
+        // const tbody = document.getElementById('contenido');
         
 
-        const tdRut = document.createElement('td');
-        tdRut.innerText = contenidoTabla.rut;
+        // formulario.addEventListener('submit', function (evento) {
+        //     const rut = formulario.rut.value;
+        //     const nombre = formulario.nombre.value;
+        //     const apellido = formulario.apellido.value;
+        //     const telefono = formulario.telefono.value;
+        //     const correoelectronico = formulario.correoelectronico.value;
+        //     const cargo = formulario.cargo.value;
+        //     const objetoPersonaTemporal = {
+        //         rut: rut,
+        //         nombre: nombre,
+        //         apellido: apellido,
+        //         telefono: telefono,
+        //         correoelectronico: correoelectronico,
+        //         cargo: cargo,
+        //     };
 
-        const tdNombre = document.createElement('td');
-        tdNombre.innerText = contenidoTabla.nombre;
+        //     anadirElementoALaListaYLocalStorange(objetoPersonaTemporal);
+        //     anadirElementoATabla(objetoPersonaTemporal);
+        //     alert('Haz sido registrado');
+        //     formulario.reset();
 
-        const tdApellido = document.createElement('td');
-        tdApellido.innerText = contenidoTabla.apellido;
-
-        const tdTelefono = document.createElement('td');
-        tdTelefono.innerText = contenidoTabla.telefono;
-
-        const tdCorreoelectronico = document.createElement('td');
-        tdCorreoelectronico.innerText = contenidoTabla.correoelectronico;
-
-        const tdCargo = document.createElement('td');
-
-        tdCargo.innerText= contenidoTabla.cargo;
-        tdCargo.addEventListener('click', function() {
-            console.log('oprimido :', contenidoTabla.cargo);
-
-        });
-
-        trTemporal.appendChild(tdRut);
-        trTemporal.appendChild(tdNombre);
-        trTemporal.appendChild(tdApellido);
-        trTemporal.appendChild(tdTelefono);
-        trTemporal.appendChild(tdCorreoelectronico);
-        trTemporal.appendChild(tdCargo);
-
-        tbody.appendChild(trTemporal);
-    }
-
-        function pintarTablaInicio() {
-            for (let i = 0; i < datosUsuario.length; i++) {
-                anadirElementoATabla(datosUsuario[i]);
-            }
-        }
-        document.addEventListener('readystatechange', function () {
-            if (document.readyState === 'interactive') {
-                pintarTablaInicio();
-            }
-
-    });
+        // });
+        // function anadirElementoALaListaYLocalStorange(persona) {
+        //     datosUsuario.push(persona);
+        //     localStorage.setItem('personas', JSON.stringify(datosUsuario));
+        // }
 
 
 
-}());
+        // function anadirElementoATabla(contenidoTabla) {
+        //     const trTemporal = document.createElement('tr');
+
+
+        //     const tdRut = document.createElement('td');
+        //     tdRut.innerText = contenidoTabla.rut;
+
+        //     const tdNombre = document.createElement('td');
+        //     tdNombre.innerText = contenidoTabla.nombre;
+
+        //     const tdApellido = document.createElement('td');
+        //     tdApellido.innerText = contenidoTabla.apellido;
+
+        //     const tdTelefono = document.createElement('td');
+        //     tdTelefono.innerText = contenidoTabla.telefono;
+
+        //     const tdCorreoelectronico = document.createElement('td');
+        //     tdCorreoelectronico.innerText = contenidoTabla.correoelectronico;
+
+        //     const tdCargo = document.createElement('td');
+
+        //     tdCargo.innerText = contenidoTabla.cargo;
+        //     tdCargo.addEventListener('click', function () {
+        //         console.log('oprimido :', contenidoTabla.cargo);
+
+        //     });
+
+        //     trTemporal.appendChild(tdRut);
+        //     trTemporal.appendChild(tdNombre);
+        //     trTemporal.appendChild(tdApellido);
+        //     trTemporal.appendChild(tdTelefono);
+        //     trTemporal.appendChild(tdCorreoelectronico);
+        //     trTemporal.appendChild(tdCargo);
+
+        //     tbody.appendChild(trTemporal);
+        // }
+
+        // function pintarTablaInicio() {
+        //     for (let i = 0; i < datosUsuario.length; i++) {
+        //         anadirElementoATabla(datosUsuario[i]);
+        //     }
+        // }
+        // document.addEventListener('readystatechange', function () {
+        //     if (document.readyState === 'interactive') {
+        //         pintarTablaInicio();
+        //     }
+
+        // });
